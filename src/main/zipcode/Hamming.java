71fd936c77cc298783549d34feb1b7982341728b
kylehwelch <welch@udel.edu>
2022-07-28 18:56:41 -0400

    it's been hammed

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
 package main.zipcode;
 
 public class Hamming {
+
+    String[] first;
+    String[] second;
+
     public Hamming(String s, String s1) {
+        first = s.split("");
+        second = s1.split("");
+        if (first.length != second.length) {
+            throw new java.lang.IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+
+        int count = 0;
+        for (int i = 0; i < first.length; i++) {
+            if (!first[i].equals(second[i])) count++;
+        }
+        return count;
     }
 }
